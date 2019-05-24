aa8cf2317d158bb97d39244d6301f78cb9b28908
hailongxiong <hailongxiong@126.com>
2019-05-24 22:45:57 +0800

    dev

--- sort/bubble_sort.cc
+++ sort/bubble_sort.cc
@@ -34,10 +34,96 @@ void bubbleSort0(int a[], int size)
     }
 }
 
+void bubbleSort1(int a[], int size)
+{
+    // main process
+    cout << "bubble sort 1" << endl;
+    for (int i = 0; i < size-1; i++)
+    {
+        int min = a[i];
+        int flag = i;
+        for (int j = i+1; j < size; j++)
+        {
+            if (min > a[j])
+            {
+                min = a[j];
+                flag = j;
+            }
+        }
+
+        if (flag != i)
+        {
+            int tmp = a[i];
+            a[i] = a[flag];
+            a[flag] = tmp;
+        }
+    }
+
+    cout << "after bubble sort value: " << endl;
+    for (int n = 0; n < 10; n++)
+    {
+        cout << "the n is " << n << "the value is " << a[n] << endl;
+    }
+}
+
+void bubbleSort2(int a[], int size)
+{
+    // real bubble
+    cout << "bubble sort2 " << endl;
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = size - 1; j > i; j--)
+        {
+            if (a[j] < a[j-1])
+            {
+                int tmp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = tmp;
+            }
+        }
+    }
+
+    cout << "after bubble sort value: " << endl;
+    for (int n = 0; n < 10; n++)
+    {
+        cout << "the n is " << n << "the value is " << a[n] << endl;
+    }
+}
+
+void bubbleSort3(int a[], int size)
+{
+    // bubble optimization
+    cout << "bubble sort3 " << endl;
+	int flag = true;
+    for (int i = 0; i < size - 1 && flag; i++)
+    {
+		int flag = false;
+        for (int j = size - 1; j > i; j--)
+        {
+            if (a[j] < a[j-1])
+            {
+                int tmp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = tmp;
+				flag = true;
+            }
+        }
+    }
+
+    cout << "after bubble sort value: " << endl;
+    for (int n = 0; n < 10; n++)
+    {
+        cout << "the n is " << n << "the value is " << a[n] << endl;
+    }
+}
+
 int main()
 {
     cout << "bubble sort " << endl;
-    bubbleSort0(a, size);
+    //bubbleSort0(a, size);
+    //bubbleSort1(a, size);
+    //bubbleSort2(a, size);
+    bubbleSort3(a, size);
 
     return 0;
 }
